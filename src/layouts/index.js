70e2c979bb52e0f93c2436ef3f413844c922a5a2
suxue <1683248243@qq.com>
2019-01-23 12:30:39 +0800

    change home Page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -25,6 +25,17 @@ class LayoutConfig extends React.PureComponent {
 				payload: {}
 			})
 		}
+		window.addEventListener('storage',(e)=>{
+			if(e.key==='sid'){
+				this.props.dispatch({
+					type: 'login/login',
+					payload: {sid:e.newValue}
+				})
+			}
+		})
+	}
+	componentWillUnmount(){
+		window.removeEventListener('storage')
 	}
 	render() {
 		console.log(this.props);
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { Menu, Carousel } from 'antd';
-import styles from './index.css';
+import styles from './index.less';
 import topPic1 from '@/assets/topPic1.jpg';
 import topPic2 from '@/assets/topPic2.jpg';
 import topPic3 from '@/assets/topPic3.jpg';
-import logo1 from '@/assets/logo1.png';
-import timeM from '@/assets/time-manager.png';
-import projectM from '@/assets/project-manager.png';
-import appPic from '@/assets/App.png'
-import webDesign from '@/assets/computer.png';
+import logo1 from '@/assets/icon.png';
+import majiangPic from '@/assets/majiang.png';
+import guojixqPic from '@/assets/gjxq1.jpg';
+import zhongguoxqPic from '@/assets/zgxq.jpg';
+import doudizhuPic from '@/assets/doudizhu.jpg';
+import { Link } from 'dva/router';
+
+
+const SubMenu = Menu.SubMenu;
 
 // console.log('------home ------',props);
 class NavigationBar extends Component {
@@ -26,49 +30,76 @@ class NavigationBar extends Component {
 
     render() {
         return (
-            <>
-                {/* 核心业务 */}
-                <div className={styles.majorTitle}>核心业务</div>
+            <React.Fragment>
+
+                {/* 时事新闻 */}
+                <div className={styles.bigNews}>
+                    时事新闻
+          <span><Link to='./news' style={{ color: 'cadetblue' }}>更多 >></Link></span>
+                </div>
+                <Carousel autoplay className={styles.toppicCarousel}>
+                    <img className={styles.toppic} src={topPic1} alt="新闻图片-1" />
+                    <img className={styles.toppic} src={topPic2} alt="新闻图片-2" />
+                    <img className={styles.toppic} src={topPic3} alt="新闻图片-3" />
+                </Carousel>
+
+                {/* 热门棋牌 */}
+                <div className={styles.majorTitle}>
+                    热门棋牌
+          <span><Link to='./teaching' style={{ color: 'cadetblue' }}>更多 >></Link></span>
+                </div>
                 <div className={styles.majorList}>
                     <div className={styles.majorDiv}>
-                        <img className={styles.majorPic} src={appPic} alt="技术推广" />
-                        <div className={styles.majorName}>APP开发</div>
-                        <div className={styles.majorDescribe}>基于React Native 研发的APP，简洁高效，一套代码多端使用，降低用户开发成本以及运维成本</div>
+                        <img className={styles.majorPic} src={majiangPic} alt="技术推广" />
+                        <div className={styles.majorName}>麻将</div>
+                        <div className={styles.majorDescribe}>麻将，起源于中国，粤港澳地区俗称麻雀，由中国古人发明的博弈游戏，娱乐用具，一般用竹子、骨头或塑料制成的小长方块，上面刻有花纹或字样，北方麻将每副136张，南方麻将多八个花牌，分别是春夏秋冬，梅竹兰菊，共计144张。</div>
                     </div>
                     <div className={styles.majorDiv}>
-                        <img className={styles.majorPic} src={webDesign} alt="网站设计" />
-                        <div className={styles.majorName}>高端网站建设</div>
-                        <div className={styles.majorDescribe}>针对用户需求，以及行业特设，定制开发，彰显客户品牌形象</div>
+                        <img className={styles.majorPic} src={zhongguoxqPic} alt="网站设计" />
+                        <div className={styles.majorName}>中国象棋</div>
+                        <div className={styles.majorDescribe}>中国象棋是起源于中国的一种棋戏，属于二人对抗性游戏的一种，在中国有着悠久的历史。由于用具简单，趣味性强，成为流行极为广泛的棋艺活动。</div>
                     </div>
                     <div className={styles.majorDiv}>
-                        <img className={styles.majorPic} src={projectM} alt="方案策划" />
-                        <div className={styles.majorName}>业务系统研发</div>
-                        <div className={styles.majorDescribe}>基于B/S架构的系统研发，让用户办公轻松自如</div>
+                        <img className={styles.majorPic} src={guojixqPic} alt="方案策划" />
+                        <div className={styles.majorName}>国际象棋</div>
+                        <div className={styles.majorDescribe}>国际象棋（Chess），又称西洋棋，是一种二人对弈的棋类游戏。棋盘为正方形，由64个黑白相间的格子组成；棋子分黑白两方共32枚，每方各16枚。虽然汉语称之为西洋棋或国际象棋，但是实际上它起源于亚洲，后由阿拉伯人传入欧洲，成为国际通行棋种，也是一项智力竞技运动，曾一度被列为奥林匹克运动会正式比赛项目。</div>
                     </div>
                     <div className={styles.majorDiv}>
-                        <img className={styles.majorPic} src={timeM} alt="ERP系统定制" />
-                        <div className={styles.majorName}>ERP系统定制</div>
-                        <div className={styles.majorDescribe}>面向各型组织的综合办公管理平台。全面实现协同工作、高效管理，打破信息孤岛，高绩效组织的统一工作平台。</div>
+                        <img className={styles.majorPic} src={doudizhuPic} alt="ERP系统定制" />
+                        <div className={styles.majorName}>斗地主</div>
+                        <div className={styles.majorDescribe}>斗地主，是一种在中国流行的纸牌游戏。游戏最少由3个玩家进行，用一副54张牌（连鬼牌），其中一方为地主，其余两家为另一方，双方对战，先出完牌的一方获胜。该扑克游戏最初流行于中国湖北武汉市汉阳区，现已逐渐在全球各地流行。</div>
 
                     </div>
                 </div>
-                {/* 优秀案例 */}
-                <div className={styles.caseTitle}>优秀案例</div>
+                {/* 最新赛事 */}
+                <div className={styles.caseTitle}>
+                    最新赛事
+          <span><Link to='./games' style={{ color: 'cadetblue' }}>更多 >></Link></span>
+                </div>
                 <div className={styles.caseList}>
                     <img className={styles.casePic} src={topPic1} alt="案例-1" />
                     <img className={styles.casePic} src={topPic2} alt="案例-2" />
                     <img className={styles.casePic} src={topPic3} alt="案例-3" />
                 </div>
-                {/* 企业介绍 */}
-                <div className={styles.companyInfoTitle}>企业介绍</div>
+                {/* 平台介绍 */}
+                <div className={styles.companyInfoTitle}>平台介绍</div>
                 <div className={styles.companyInfo}>
                     <img className={styles.companyInfoPic} src={logo1} alt="公司LOGO" />
-                    <h2>北京欧德慧通信息技术有限公司</h2>
-                    <p style={{ textIndent: '2em' }}>北京欧德慧通信息技术有限公司成立于2015年，注册资金100万，是一家有着丰富项目经验的IT产业公司。公司服务于各个行业，提供技术开发，技术推广，技术转让，技术咨询以及技术服务，销售公司自行开发的产品以及提供计算机系统服务，基础软件服务，应用软件服务。
-                    多年以来本着“诚实做人，踏实做事，用心服务”的宗旨，通过我们的专业水平和不懈努力,三年来，一直秉承以用户需求为核心，在专注互联网市场开拓的同时,为多家企业提供技术服务，优质、用心的服务赢得了众多企业的信赖和好评，逐渐树立起公司良好品牌。并且完善了售后服务体系。我们相信,通过我们的不断努力和追求，一定能够实现与各个企业的互利共赢。
+                    <h2>智赛棋牌竞技平台</h2>
+                    <p style={{ textIndent: '2em' }}>
+                        “智赛竞技平台”系统是一款比赛娱乐双模块的棋牌竞技类产品。平台的长远规划不仅包含了桥牌、中国象棋、国际象棋、围棋、国际跳棋，后期还会麻将、斗地主、五子棋等当下热门的热门民间娱乐项目。<br />
+                        项目一期重点是桥牌项目的研发推广，本平台桥牌项目的最大特点是解决了当前线下桥牌竞技无法在线上完美呈现的问题。 平台口号：<br />
+                        <i style={{ textIndent: '2em', color: 'blue', fontSize: '16px', fontWeight: 'bolder' }}>便捷安全的线上享受,  原汁原味的线下体验</i><br />
+                        平台三大优势：<br />
+                        * 更贴合实际的竞技体验<br />
+                        --- 打牌与视频相结合，100%还原线下<br />
+                        * 更高的竞技性<br />
+                        --- 高标准的竞技模式，低门槛的参赛标准<br />
+                        * 更公平的竞技环境<br />
+                        --- 大广角摄像头无死角监控，比赛期间工作人员实时监督，保证比赛公平公正<br />
                     </p>
                 </div>
-            </>
+            </React.Fragment>
         );
     }
 }
